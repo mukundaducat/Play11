67a17deb9cf9157485542f7d605a60195b5bee30
Rabindranath Bisoyi <rabindranath@singularislabs.com>
2024-03-15 21:57:41 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Play11.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        
+        public ActionResult Index2()
+        {
+            return View();
+        }
+        
+        public ActionResult page()
+        {
+            return View();
+        }
+        public ActionResult register()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Play11.MvcApplication" Language="C#" %>
